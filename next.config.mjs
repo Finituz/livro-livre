5a4119353ad5cc1e50cf4ddfa8387551ac357d76
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-03-30 00:15:13 -0300

    fix export

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
+const nextConfig = {
   /**
    * Enable static exports for the App Router.
    *
