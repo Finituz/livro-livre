958ed9a4480e8ff6c8b9785035006c3ce412ffa4
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-03-30 12:59:57 -0300

    nextauth no reading secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,8 +6,8 @@ import { Session } from "next-auth";
 const options: NextAuthOptions = {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      clientId: process.env.AUTH_GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET ?? "",
       authorization: {
         params: {
           scope:
