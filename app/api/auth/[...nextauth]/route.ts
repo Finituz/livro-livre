944e15eae6764d21458e667a6bbc6561418608ff
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2025-03-30 19:34:39 -0300

    fix 401 connect to google api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,6 +14,23 @@ const authOptions: AuthOptions = {
       },
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = String(token.accessToken);
+      session.refreshToken = String(token.refreshToken);
+      return session;
+    },
+  },
 };
 
 const handlers = NextAuth(authOptions);
